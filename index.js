d5d6511a272d8f9753daf6a5d056c67ac0f3bb16
mouasit <mustaphaouassit60@gmail.com>
2023-10-13 16:14:12 +0100

    add chose from dropdown

--- index.js
+++ index.js
@@ -10,15 +10,34 @@ function closeModal() {
   modal.classList.add("hidden");
 }
 
-function openDropDownCPU(element) {
+function toggleDropDownCPU(element) {
   dropDownCPU.classList.toggle("hidden");
   element.querySelector("i").classList.toggle("bx-chevron-down");
   element.querySelector("i").classList.toggle("bx-chevron-up");
 }
 
-function openDropDownList(element) {
+function toggleDropDownList(element) {
   dropDownList.classList.toggle("hidden");
   element.querySelector("i").classList.toggle("bx-chevron-down");
   element.querySelector("i").classList.toggle("bx-chevron-up");
 }
 
+dropDownCPU.querySelectorAll("button").forEach((element) => {
+  element.addEventListener("click", () => {
+    const buttonDropdown = dropDownCPU.parentElement.querySelector("button");
+    const valueDropDown = buttonDropdown.querySelector("span");
+    valueDropDown.innerHTML = element.innerHTML;
+
+    toggleDropDownCPU(buttonDropdown);
+  });
+});
+
+dropDownList.querySelectorAll("button").forEach((element) => {
+  element.addEventListener("click", () => {
+    const buttonDropdown = dropDownList.parentElement.querySelector("button");
+    const valueDropDown = buttonDropdown.querySelector("span");
+    valueDropDown.innerHTML = element.innerHTML;
+
+    toggleDropDownList(buttonDropdown);
+  });
+});
